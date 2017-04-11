08fdc634e1e1a93a1e6214c72d7004a0a27de3f1
Brian McFarlane <bmcfarlane82@gmail.com>
2017-04-10 18:53:29 -0700

    All salmon locations are printing to browswer. Working on CSS stuff

--- app.js
+++ app.js
@@ -7,9 +7,16 @@
 
 // Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
 //
+var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ']
 
-var firstandpikeUl = document.getElementById('firstandpike');
+var locFirstAndPikeUl = document.getElementById('firstandpike');
+var locSeaTacAirportUl = document.getElementById('seatacairport');
+var locSeattleCenterUl = document.getElementById('seattlecenter');
+var locCapitolHillUl = document.getElementById('capitolhill');
+var locAlkiUl = document.getElementById('alki');
 
+
+// First and Pike Start
 var locFirstAndPike = {
   minCustomerHour: 23,
   maxCustomerHour: 65,
@@ -24,11 +31,14 @@ var firstandpikeLi;
 for (var i = 0; i < 15; i++) {
   var cookiePerHour = (locFirstAndPike.randomCustomer() * locFirstAndPike.advCustomerCookie);
   cookiePerHour = Math.floor(cookiePerHour);
-  locFirstAndPike.simCookies.push(cookiePerHour);
+  locFirstAndPike.simCookies.push(hours[i] + cookiePerHour +' cookies');
   firstandpikeLi = document.createElement('li');
   firstandpikeLi.textContent = locFirstAndPike.simCookies[i];
-  firstandpikeUl.appendChild(firstandpikeLi);
+  locFirstAndPikeUl.appendChild(firstandpikeLi);
 }
+
+//First and Pike End
+// SeaTac Airport Start
 var locSeaTacAirport = {
   minCustomerHour: 3,
   maxCustomerHour: 24,
@@ -39,11 +49,19 @@ var locSeaTacAirport = {
     return randomNumber;
   }
 }
+var SeaTacAirportLi;
 for (var i = 0; i < 15; i++) {
   var cookiePerHour = (locSeaTacAirport.randomCustomer() * locSeaTacAirport.advCustomerCookie);
   cookiePerHour = Math.floor(cookiePerHour)
-  locSeaTacAirport.simCookies.push(cookiePerHour)
+  locSeaTacAirport.simCookies.push(hours[i] + cookiePerHour +' cookies');
+  SeaTacAirportLi = document.createElement('li');
+  SeaTacAirportLi.textContent = locSeaTacAirport.simCookies[i];
+  locSeaTacAirportUl.appendChild(SeaTacAirportLi);
 }
+
+//SeaTac Airport end
+// Seattle Center Start
+
 var locSeattleCenter = {
   minCustomerHour: 11,
   maxCustomerHour: 38,
@@ -54,11 +72,19 @@ var locSeattleCenter = {
     return randomNumber;
   }
 };
+var SeattleCenterLi;
 for (var i = 0; i < 15; i++) {
   var cookiePerHour = (locSeattleCenter.randomCustomer() * locSeattleCenter.advCustomerCookie);
   cookiePerHour = Math.floor(cookiePerHour)
-  locSeattleCenter.simCookies.push(cookiePerHour)
+  locSeattleCenter.simCookies.push(hours[i] + cookiePerHour +' cookies');
+  SeattleCenterLi = document.createElement('li');
+  SeattleCenterLi.textContent = locSeattleCenter.simCookies[i];
+  locSeattleCenterUl.appendChild(SeattleCenterLi);
 }
+
+//Seattle Center End
+//Capital Hill Start
+
 var locCapitolHill = {
   minCustomerHour: 20,
   maxCustomerHour: 38,
@@ -69,11 +95,19 @@ var locCapitolHill = {
     return randomNumber;
   }
 };
+var capitolHillLi;
 for (var i = 0; i < 15; i++) {
   var cookiePerHour = (locCapitolHill.randomCustomer() * locCapitolHill.advCustomerCookie);
   cookiePerHour = Math.floor(cookiePerHour)
-  locCapitolHill.simCookies.push(cookiePerHour)
+  locCapitolHill.simCookies.push(hours[i] + cookiePerHour +' cookies');
+  capitolHillLi = document.createElement('li');
+  capitolHillLi.textContent = locCapitolHill.simCookies[i];
+  locCapitolHillUl.appendChild(capitolHillLi);
 }
+
+//Capital Hill end
+//Alki start
+
 var locAlki = {
   minCustomerHour: 2,
   maxCustomerHour: 16,
@@ -84,12 +118,17 @@ var locAlki = {
     return randomNumber;
   }
 };
+var alkiLi;
 for (var i = 0; i < 15; i++) {
   var cookiePerHour = (locAlki.randomCustomer() * locAlki.advCustomerCookie);
   cookiePerHour = Math.floor(cookiePerHour)
-  locAlki.simCookies.push(cookiePerHour)
+  locAlki.simCookies.push(hours[i] + cookiePerHour +' cookies');
+  alkiLi = document.createElement('li');
+  alkiLi.textContent = locAlki.simCookies[i];
+  locAlkiUl.appendChild(alkiLi);
 }
 
+//Alki End
 
 // Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
 //
