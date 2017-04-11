6340f3cb4c0ad8513e3fa50cfb45c3e15ff06c85
Brian McFarlane <bmcfarlane82@gmail.com>
2017-04-10 17:01:14 -0700

    adv cookies sold arry output finished

--- app.js
+++ app.js
@@ -11,47 +11,78 @@ var locFirstAndPike = {
   minCustomerHour: 23,
   maxCustomerHour: 65,
   advCustomerCookie: 6.3,
-  randomCustomer: function() {
-    var randomNumber = Math.floor(Math.random() * (this.minCustomerHour + this.maxCustomerHour)) + 1;
+  simCookies: [],
+  randomCustomer: function(min, max) {
+    var randomNumber = Math.floor(Math.random() * (this.maxCustomerHour - this.minCustomerHour)) + this.minCustomerHour;
     return randomNumber;
   }
 };
+for (var i = 0; i < 15; i++) {
+  var cookiePerHour = (locFirstAndPike.randomCustomer() * locFirstAndPike.advCustomerCookie);
+  cookiePerHour = Math.floor(cookiePerHour)
+  locFirstAndPike.simCookies.push(cookiePerHour)
+}
 var locSeaTacAirport = {
   minCustomerHour: 3,
   maxCustomerHour: 24,
   advCustomerCookie: 1.2,
+  simCookies: [],
   randomCustomer: function() {
-    var randomNumber = Math.floor(Math.random() * (this.minCustomerHour + this.maxCustomerHour)) + 1;
+    var randomNumber = Math.floor(Math.random() * (this.maxCustomerHour - this.minCustomerHour)) + this.minCustomerHour;
     return randomNumber;
   }
+}
+for (var i = 0; i < 15; i++) {
+  var cookiePerHour = (locSeaTacAirport.randomCustomer() * locSeaTacAirport.advCustomerCookie);
+  cookiePerHour = Math.floor(cookiePerHour)
+  locSeaTacAirport.simCookies.push(cookiePerHour)
 }
 var locSeattleCenter = {
   minCustomerHour: 11,
   maxCustomerHour: 38,
-  advCustomerCookie: 3.7
+  advCustomerCookie: 3.7,
+  simCookies: [],
   randomCustomer: function() {
-    var randomNumber = Math.floor(Math.random() * (this.minCustomerHour + this.maxCustomerHour)) + 1;
+    var randomNumber = Math.floor(Math.random() * (this.maxCustomerHour - this.minCustomerHour)) + this.minCustomerHour;
     return randomNumber;
   }
 };
+for (var i = 0; i < 15; i++) {
+  var cookiePerHour = (locSeattleCenter.randomCustomer() * locSeattleCenter.advCustomerCookie);
+  cookiePerHour = Math.floor(cookiePerHour)
+  locSeattleCenter.simCookies.push(cookiePerHour)
+}
 var locCapitolHill = {
   minCustomerHour: 20,
   maxCustomerHour: 38,
   advCustomerCookie: 2.3,
+  simCookies: [],
   randomCustomer: function() {
-    var randomNumber = Math.floor(Math.random() * (this.minCustomerHour + this.maxCustomerHour)) + 1;
+    var randomNumber = Math.floor(Math.random() * (this.maxCustomerHour - this.minCustomerHour)) + this.minCustomerHour;
     return randomNumber;
   }
 };
+for (var i = 0; i < 15; i++) {
+  var cookiePerHour = (locCapitolHill.randomCustomer() * locCapitolHill.advCustomerCookie);
+  cookiePerHour = Math.floor(cookiePerHour)
+  locCapitolHill.simCookies.push(cookiePerHour)
+}
 var locAlki = {
   minCustomerHour: 2,
   maxCustomerHour: 16,
-  advCustomerCookie: 4.6
+  advCustomerCookie: 4.6,
+  simCookies: [],
   randomCustomer: function() {
     var randomNumber = Math.floor(Math.random() * (this.minCustomerHour + this.maxCustomerHour)) + 1;
     return randomNumber;
   }
 };
+for (var i = 0; i < 15; i++) {
+  var cookiePerHour = (locAlki.randomCustomer() * locAlki.advCustomerCookie);
+  cookiePerHour = Math.floor(cookiePerHour)
+  locAlki.simCookies.push(cookiePerHour)
+}
+
 
 // Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
 //
