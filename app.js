451d42600e643939c9c7ea327844c3a2d08d29cb
Brian McFarlane <bmcfarlane82@gmail.com>
2017-04-12 17:46:43 -0700

    switching branches

--- app.js
+++ app.js
@@ -2,60 +2,70 @@
 
 var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: '];
 
-var locFirstAndPikeTr = document.getElementById('firstandpike');
-var locSeaTacAirportTr = document.getElementById('seatacairport');
-var locSeattleCenterTr = document.getElementById('seattlecenter');
-var locCapitolHillTr = document.getElementById('capitolhill');
-var locAlkiTr = document.getElementById('alki');
+var tablelocation = document.getElementById('cooltablebro');
 
-var storesTr = [locFirstAndPikeTr, locSeaTacAirportTr, locSeattleCenterTr, locCapitolHillTr, locAlkiTr];
 
-function CookieStore (minCustomerHour, maxCustomerHour, advCustomerCookie, simCookies) {
+function CookieStore (thisStoreName, minCustomerHour, maxCustomerHour, advCustomerCookie) {
+  this.thisStoreName = thisStoreName;
   this.minCustomerHour = minCustomerHour;
   this.maxCustomerHour = maxCustomerHour;
   this.advCustomerCookie = advCustomerCookie;
-  this.simCookies = simCookies;
+  this.simCookies = [];
 }
 
 CookieStore.prototype.randomCustomer = function() {
-  this.ranNumber = Math.floor(Math.random() * (this.maxCustomerHour - this.minCustomerHour)) + this.minCustomerHour;
+  for (var j = 0; j < hours.length; j++) {
+    var ranNumber = Math.floor(Math.random() * (this.maxCustomerHour - this.minCustomerHour)) + this.minCustomerHour;
+    var cookiePerHour;
+    cookiePerHour = ranNumber * this.advCustomerCookie;
+    cookiePerHour = Math.floor(cookiePerHour);
+    this.simCookies.push(cookiePerHour + ' cookies');
+  }
 };
 
-var locFirstAndPike = new CookieStore (23, 65, 6.3, []);
-var locSeaTacAirport = new CookieStore (3, 24, 1.2, []);
-var locSeattleCenter = new CookieStore (11, 38, 3.7, []);
-var locCapitolHill = new CookieStore (20, 38, 2.3, []);
-var locAlki = new CookieStore (2, 16, 4.6, []);
+var locFirstAndPike = new CookieStore ('1st and Pike', 23, 65, 6.3);
+var locSeaTacAirport = new CookieStore ('SeaTac Airport', 3, 24, 1.2);
+var locSeattleCenter = new CookieStore ('Seattle Center', 11, 38, 3.7);
+var locCapitolHill = new CookieStore ('Capitol Hill', 20, 38, 2.3);
+var locAlki = new CookieStore ('Alki', 2, 16, 4.6);
 
 var storeLocations = [locFirstAndPike, locSeaTacAirport, locSeattleCenter, locCapitolHill, locAlki];
 
 for (var i = 0; i < storeLocations.length; i++) {
-  for (var j = 0; j < hours.length; j++) {
-    var cookiePerHour;
-    storeLocations[i].randomCustomer();
-    cookiePerHour = storeLocations[i].ranNumber * storeLocations[i].advCustomerCookie;
-    cookiePerHour = Math.floor(cookiePerHour);
-    storeLocations[i].simCookies.push(cookiePerHour + ' cookies');
-  }
+  storeLocations[i].randomCustomer();
 }
 
 var storeLocTr;
 var storeLocTd;
-for (var x = 0; x < storeLocations.length; x++) {
-  for (var y = 0; y < hours.length; y++) {
-    storeLocTr = document.createElement('td');
-    storeLocTd = document.createElement('td');
-    storeLocTd.textContent = hours[y];
-    storeLocTr.textContent = storeLocations[x].simCookies[y];
-    storesTr[x].appendChild(storeLocTd);
-    storesTr[x].appendChild(storeLocTr);
+CookieStore.prototype.writetopage = function() {
+  for (var x = 0; x < storeLocations.length; x++) {
+    for (var y = 0; y < hours.length; y++) {
+      storeLocTr = document.createElement('td');
+      storeLocTd = document.createElement('td');
+      storeLocTd.textContent = hours[y];
+      storeLocTr.textContent = storeLocations[x].simCookies[y];
+      storesTr[x].appendChild(storeLocTd);
+      storesTr[x].appendChild(storeLocTr);
+    }
   }
+};
+for (var p = 0; p < storeLocations.length; p++) {
+  storeLocations[p].writetopage();
 }
 function handleProductCreateSubmit(event) {
   event.preventDefault();
-  console.log('wut wut');
+  var form = event.target;
+  var formStoreName = form.storeName.value;
+  var formminCustomerHour = form.minCustomerHour.value;
+  var formmaxCustomerHour = form.maxCustomerHour.value;
+  var formadvCustomerCookie = form.advCustomerCookie.value;
+  console.log(formStoreName, formminCustomerHour, formmaxCustomerHour, formadvCustomerCookie);
+  var newStore = new CookieStore(formStoreName, formminCustomerHour, formmaxCustomerHour, formadvCustomerCookie);
+  console.log(newStore);
+  storeLocations.push(newStore);
+  newStore.randomCustomer();
+  newStore.writetopage();
 }
 
 var createStore = document.getElementById('add-store');
-
 createStore.addEventListener('submit', handleProductCreateSubmit);
